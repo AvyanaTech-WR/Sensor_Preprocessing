9305b42b6844c1ac753b07fb7d5d95d367604d7e
REGATTE <ashok_a380@icloud.com>
2023-05-26 06:21:47 +0000

    Odom_EKF working
    FIxed issues of not publishing data

--- odom_ekf/launch/odom_ekf.launch.py
+++ odom_ekf/launch/odom_ekf.launch.py
@@ -2,21 +2,20 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     pkg_share = FindPackageShare(package='odom_ekf').find('odom_ekf')
     return LaunchDescription([
-        DeclareLaunchArgument(name='use_sim_time', default_value='True', description='Flag to enable use_sim_time'),
         Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
-            parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],
-            remappings=[('/odometry/filtered', '/odom/processed')]
+            parameters=[os.path.join(get_package_share_directory("odom_ekf"), 'config', 'ekf.yaml')],
+            remappings=[('/odometry/filtered', '/odom/processed')],
+            arguments=['--ros-args', '--log-level', 'debug']
         )
     ])
 
